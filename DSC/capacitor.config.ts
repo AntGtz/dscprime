d0820e3e3a27f84aeebc145ee692e88da8af59e4
AntGtz <tranetrose@gmail.com>
2024-10-04 10:56:03 -0600

    first commit

---
+++ DSC/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DSC',
+  webDir: 'www'
+};
+
+export default config;
